a80c75a769a94d8fd2b2f4124b1e847629b921e2
Franciszek Job <jellybean@onet,eu>
2020-12-29 20:26:56 +0100

    Updates

---
+++ precache-manifest.5a271b6ac8032c7befbdfbbea97512c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc21886448e94ff59752e5c1981282f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60d9714cdc093714fe60",
+    "url": "/static/css/main.5ea2d2de.chunk.css"
+  },
+  {
+    "revision": "4e2584e488034cafe98d",
+    "url": "/static/js/2.9f9b0d67.chunk.js"
+  },
+  {
+    "revision": "d83597d775d28f3887d670035b7da24f",
+    "url": "/static/js/2.9f9b0d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d9714cdc093714fe60",
+    "url": "/static/js/main.49b2fc1b.chunk.js"
+  },
+  {
+    "revision": "762a9d48eeff135c43cf",
+    "url": "/static/js/runtime-main.c6c6b9ad.js"
+  },
+  {
+    "revision": "c16de4351c61b376743b786eef97f17c",
+    "url": "/static/media/bckg.c16de435.png"
+  },
+  {
+    "revision": "e8a4e96bff89cb8b1d4abc6086fa4a77",
+    "url": "/static/media/flutter.e8a4e96b.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/js.0cded3a3.png"
+  },
+  {
+    "revision": "b3367de3086699bdd53570c2108ea813",
+    "url": "/static/media/node.b3367de3.png"
+  },
+  {
+    "revision": "176feecb1e58f550a61f4061c12285df",
+    "url": "/static/media/python.176feecb.png"
+  },
+  {
+    "revision": "4c744181fe96dbb99fdb31e58267f933",
+    "url": "/static/media/rpi.4c744181.svg"
+  }
+]);
